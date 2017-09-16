953071e7304f9ce2f6346ed3ab2d0163c42763c0
brian <brian@abc.com>
2017-09-16 00:07:58 -0400

    Adding bjne.

--- comenv/i3/run-no-machine.sh
+++ comenv/i3/run-no-machine.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-IMAGE=i3
+IMAGE=i3-bjne
 
 docker run -d \
   -p 4000:4000 \
-  --name desk2 \
+  --name desk3 \
   -e PASSWORD=password \
   -e USER=user \
   --cap-add=SYS_PTRACE \
