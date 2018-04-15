c0f428f37e8499a5c19d870e73c7b0aa052f9675
Brian Hamilton <brian@abc.com>
2018-04-15 06:13:08 +0000

    Adding suckless terminal (st).

--- comenv/i3/nxserver.sh
+++ comenv/i3/nxserver.sh
@@ -2,7 +2,7 @@
 groupadd -r $USER -g 433 \
 && useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
 && adduser $USER sudo \
-&& mkdir /home/$USER \
+&& mkdir -p /home/$USER \
 && chown -R $USER:$USER /home/$USER \
 && echo $USER':'$PASSWORD | chpasswd
 /etc/NX/nxserver --startup
--- comenv/i3/run-no-machine.sh
+++ comenv/i3/run-no-machine.sh
@@ -5,8 +5,9 @@ IMAGE=nes-emu-dev-i3
 docker run -d \
   -p 4000:4000 \
   --name desk3 \
-  -e PASSWORD=password \
+  -e PASSWORD=zaksab37 \
   -e USER=user \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   --cap-add=SYS_PTRACE \
   --cap-add=NET_ADMIN \
   ${IMAGE}
