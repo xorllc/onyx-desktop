953e60de6242cb5af8e2f47bc9d0cb075f8bb653
Brian Hamilton <brian@abc.com>
2018-06-19 22:47:37 -0400

    Removing nx, adding host-level video/audio/input device mapping.

--- comenv/i3/run-nx-ssh.sh
+++ comenv/i3/run-nx-ssh.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 IMAGE=nx-ssh
+xhost +"local:docker@"
 
 docker rm -f nx-ssh
 docker run -d \
-  -p 4001:4000 \
   -p 2222:22 \
   --name nx-ssh \
-  -e PASSWORD=zaksab37 \
-  -e USER=user \
+  --group-add $(getent group audio | cut -d: -f3) \
+  -e DISPLAY \
+  -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+  -e ${XAUTHORITY}:/root/.Xauthority \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native:Z \
+  -v ${XAUTHORITY}:/root/.Xauthority \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --shm-size=2g \
+  --device /dev/dri \
+  --device /dev/snd \
+  --device /dev/input \
   --cap-add=SYS_PTRACE \
   --cap-add=NET_ADMIN \
   ${IMAGE}
